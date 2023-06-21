bed05a2293c6a33cebc2aaca10b34c78d2c06b9f
Dmeet6401 <68276925+Dmeet6401@users.noreply.github.com>
2023-06-21 17:54:35 +0530

    Update cashe_decorator.py

--- cashe_decorator.py
+++ cashe_decorator.py
@@ -17,17 +17,33 @@ def memoize(func):
     return wrapper
 
 
+
 @memoize
 def fibonacci(n):
     if n<2:
         return n
     return fibonacci(n-1) + fibonacci(n-2)
 
+@memoize
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
 
 
 if __name__ == "__main__":
     start = perf_counter()
     f = fibonacci(80)
-    end = perf_counter()
+    end = perf_counter() 
     print(f)
     print(f"{end - start}")
+    # took 0.00028150000000000397 to calculate
+
+    start = perf_counter()
+    fa = factorial(50)
+    end = perf_counter() 
+    print(fa)
+    print(f"{end - start}")
+
+    # O/p = 30414093201713378043612608166064768844377641568960512000000000000
+    #Time = 0.00017790000000000167
